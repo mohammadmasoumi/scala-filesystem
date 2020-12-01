3a0e4a245ae2658da72345d03f906beed759ccd3
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-01 14:00:39 +0330

    add mkdir command

--- src/com/mohammadmasoumi/scala/commands/Mkdir.scala
+++ src/com/mohammadmasoumi/scala/commands/Mkdir.scala
@@ -1,9 +1,15 @@
 package com.mohammadmasoumi.scala.commands
 
+import com.mohammadmasoumi.scala.files.Directory
 import com.mohammadmasoumi.scala.filesystem.State
 
 class Mkdir(name: String) extends Command {
 
-  override def apply(state: State): State = ???
+  override def apply(state: State): State = {
+    val wd = state.wd
+
+    if (wd.hasEntry(name)) state.setMessage("Entry " + name + " already exists!")
+    else if (name.contains(Directory.SEPARATOR)) state.setMessage("Must no contain separators!")
+  }
 
 }
--- src/com/mohammadmasoumi/scala/files/Directory.scala
+++ src/com/mohammadmasoumi/scala/files/Directory.scala
@@ -3,6 +3,8 @@ package com.mohammadmasoumi.scala.files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
 
+  def hasEntry(name: String): Boolean = ???
+
 }
 
 object Directory {

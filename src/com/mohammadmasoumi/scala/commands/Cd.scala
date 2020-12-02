6479032c396f32f8e09a59ca7650e2c875d4b0ff
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-03 01:58:36 +0330

    init change directory command

---
+++ src/com/mohammadmasoumi/scala/commands/Cd.scala
@@ -0,0 +1,9 @@
+package com.mohammadmasoumi.scala.commands
+
+import com.mohammadmasoumi.scala.filesystem.State
+
+class Cd(dir: String) extends Command {
+
+  override def apply(state: State): State = ???
+
+}

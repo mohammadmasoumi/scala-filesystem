2d2e9f3e148f80f5ce8f5e9032e484ad0ae86e05
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-03 01:00:26 +0330

    finalize LS command

--- src/com/mohammadmasoumi/scala/commands/Ls.scala
+++ src/com/mohammadmasoumi/scala/commands/Ls.scala
@@ -5,17 +5,17 @@ import com.mohammadmasoumi.scala.filesystem.State
 
 class Ls extends Command {
 
-  def createNiceOutput(contents: List[DirEntry]): String =
-    if (contents.isEmpty) ""
-    else {
-      val entry = contents.head
-      entry.name + s"{$entry.getType}" + createNiceOutput(contents.tail) + "\n"
-    }
-
   override def apply(state: State): State = {
     val contents = state.wd.contents
     val niceOutput = createNiceOutput(contents)
     state.setMessage(niceOutput)
   }
+
+  def createNiceOutput(contents: List[DirEntry]): String =
+    if (contents.isEmpty) ""
+    else {
+      val entry = contents.head
+      entry.name + s"[${entry.getType}]" + createNiceOutput(contents.tail) + "\n"
+    }
 
 }

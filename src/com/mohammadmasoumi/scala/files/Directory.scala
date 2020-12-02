50ed74fddbe404cff3054edff8c28ec95e49f1ac
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-02 23:57:50 +0330

    complete addEntry method

--- src/com/mohammadmasoumi/scala/files/Directory.scala
+++ src/com/mohammadmasoumi/scala/files/Directory.scala
@@ -28,7 +28,8 @@ class Directory(override val parentPath: String, override val name: String, val 
     if (path.isEmpty) this
     else findEntry(path.head).asDirectory.findDescendant(path.tail)
 
-  def addEntry(newEntry: DirEntry): Directory = ???
+  def addEntry(newEntry: DirEntry): Directory =
+    new Directory(parentPath, name, contents :+ newEntry)
 
   override def asDirectory: Directory = this
 }

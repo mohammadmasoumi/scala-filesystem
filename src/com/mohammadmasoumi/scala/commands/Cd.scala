37126fb6fff9e6ac4ddf1dd6937fa46c91c413d4
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-03 02:02:29 +0330

    breakdown the taskg

--- src/com/mohammadmasoumi/scala/commands/Cd.scala
+++ src/com/mohammadmasoumi/scala/commands/Cd.scala
@@ -4,6 +4,20 @@ import com.mohammadmasoumi.scala.filesystem.State
 
 class Cd(dir: String) extends Command {
 
-  override def apply(state: State): State = ???
+  override def apply(state: State): State = {
+
+    // 1. find root
+    val root = state.root
+    val wd = state.wd
+
+    // 2. find the absolute path of the directory I want to cd to
+
+
+    // 3. find the directory to cd to, given the path
+
+    // 4. change the state - given the new directory
+
+
+  }
 
 }

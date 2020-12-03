cb9c1a3afaeadf0ea4b4e55b27db3b2efb010eb3
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-03 23:44:20 +0330

    add Cd Command into Commands

--- src/com/mohammadmasoumi/scala/commands/Command.scala
+++ src/com/mohammadmasoumi/scala/commands/Command.scala
@@ -17,6 +17,7 @@ object Command {
   val LS = "ls"
   val PWD = "pwd"
   val TOUCH = "touch"
+  val CD = "cd"
 
   def emptyCommand: Command = new Command { // anonymous class
     override def apply(state: State): State = state
@@ -43,6 +44,9 @@ object Command {
     else if (TOUCH.equals(tokens(0)))
       if (tokens.length < 2) incompleteCommand(TOUCH)
       else new Touch(tokens(1))
+    else if (CD.equals(tokens(1)))
+      if (tokens.length < 2) incompleteCommand(TOUCH)
+      else new Cd(tokens(1))
     else new UnknownCommand
   }
 

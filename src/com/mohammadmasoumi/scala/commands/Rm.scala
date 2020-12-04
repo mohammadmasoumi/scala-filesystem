7e6bde8f3248f9cc443002cb13a48d6dfaaca6ed
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-04 22:21:54 +0330

    add doRm

--- src/com/mohammadmasoumi/scala/commands/Rm.scala
+++ src/com/mohammadmasoumi/scala/commands/Rm.scala
@@ -1,17 +1,33 @@
 package com.mohammadmasoumi.scala.commands
+import com.mohammadmasoumi.scala.files.Directory
 import com.mohammadmasoumi.scala.filesystem.State
 
 class Rm(name: String) extends Command{
+
+
   override def apply(state: State): State = {
-    // 1. get working dir
+
+    // 1. get current working dir
+    val wd = state.wd
 
     // 2. get absolute path
+    val absolutePath =
+      if (name.startsWith(Directory.SEPARATOR)) name
+      else if (wd.isRoot) wd.path + name
+      else wd.path + Directory.SEPARATOR + name
 
     // 3. do some checks
+    if (Directory.ROOT_PATH.equals(absolutePath))
+      state.setMessage("Not supported yet!")
+    else
+      doRm(state, absolutePath)
 
+  }
+
+  def doRm(state: State, path: String): State = {
     // 4. find the entry to remove
 
     // 5. update structure like we did for mkdir
-
   }
+
 }

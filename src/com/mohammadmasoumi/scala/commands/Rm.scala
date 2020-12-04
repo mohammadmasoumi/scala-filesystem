bfd12b0329dd0cebe8a9baca51b7f80ce3bea2f4
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-04 22:48:29 +0330

    update Rm command

--- src/com/mohammadmasoumi/scala/commands/Rm.scala
+++ src/com/mohammadmasoumi/scala/commands/Rm.scala
@@ -1,12 +1,12 @@
 package com.mohammadmasoumi.scala.commands
+
 import com.mohammadmasoumi.scala.files.Directory
 import com.mohammadmasoumi.scala.filesystem.State
 
-class Rm(name: String) extends Command{
+class Rm(name: String) extends Command {
 
 
   override def apply(state: State): State = {
-
     // 1. get current working dir
     val wd = state.wd
 
@@ -21,7 +21,6 @@ class Rm(name: String) extends Command{
       state.setMessage("Not supported yet!")
     else
       doRm(state, absolutePath)
-
   }
 
   def doRm(state: State, path: String): State = {

582c81a17140b8c6d2f9496bf75135a1deab4a3d
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-04 23:15:20 +0330

    complete functions

--- src/com/mohammadmasoumi/scala/files/Directory.scala
+++ src/com/mohammadmasoumi/scala/files/Directory.scala
@@ -10,7 +10,9 @@ class Directory(override val parentPath: String, override val name: String, val 
   def addEntry(newEntry: DirEntry): Directory =
     new Directory(parentPath, name, contents :+ newEntry)
 
-  def removeEntry(head: String) = ???
+  def removeEntry(entryName: String): Directory =
+    if (!hasEntry(entryName)) this
+    else new Directory(parentPath, name, contents.filter(x => !x.name.equals(entryName)))
 
   def findEntry(entryName: String): DirEntry = {
     @tailrec
@@ -35,7 +37,9 @@ class Directory(override val parentPath: String, override val name: String, val 
     if (path.isEmpty) this
     else findEntry(path.head).asDirectory.findDescendant(path.tail)
 
-  def findDescendant(relativePath: String): Directory = ???
+  def findDescendant(relativePath: String): Directory =
+    if (relativePath.isEmpty) this
+    else findDescendant(relativePath.split(Directory.SEPARATOR).toList)
 
   def getAllFoldersInPath: List[String] =
   // /a/b/c/d => List["a","b","c","d"]

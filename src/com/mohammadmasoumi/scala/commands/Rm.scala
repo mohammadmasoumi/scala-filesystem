b0d1088746a07bb99cb2666d825de30667c89099
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-04 22:16:31 +0330

    add rm command

---
+++ src/com/mohammadmasoumi/scala/commands/Rm.scala
@@ -0,0 +1,17 @@
+package com.mohammadmasoumi.scala.commands
+import com.mohammadmasoumi.scala.filesystem.State
+
+class Rm(name: String) extends Command{
+  override def apply(state: State): State = {
+    // 1. get working dir
+
+    // 2. get absolute path
+
+    // 3. do some checks
+
+    // 4. find the entry to remove
+
+    // 5. update structure like we did for mkdir
+
+  }
+}

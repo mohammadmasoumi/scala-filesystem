c3e1be07b6aada88f5c6840a82b16c7f31166620
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-06 12:31:50 +0330

    final commit

--- src/com/mohammadmasoumi/scala/filesystem/FileSystem.scala
+++ src/com/mohammadmasoumi/scala/filesystem/FileSystem.scala
@@ -1,22 +1,24 @@
 package com.mohammadmasoumi.scala.filesystem
 
+import java.util.Scanner
+
 import com.mohammadmasoumi.scala.commands.Command
 import com.mohammadmasoumi.scala.files.Directory
 
 object FileSystem extends App {
 
   val root = Directory.ROOT
-  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
-    currentState.show
-    Command.from(newLine)(currentState)
-  })
+//  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+//    currentState.show
+//    Command.from(newLine)(currentState)
+//  })
 
-  //  var state = State(root, root)
-  //  val scanner = new Scanner(System.in)
-  //
-  //  while (true) {
-  //    state.show
-  //    val input = scanner.nextLine()
-  //    state = Command.from(input)(state) // state full program
-  //  }
+    var state = State(root, root)
+    val scanner = new Scanner(System.in)
+
+    while (true) {
+      state.show
+      val input = scanner.nextLine()
+      state = Command.from(input)(state) // state full program
+    }
 }

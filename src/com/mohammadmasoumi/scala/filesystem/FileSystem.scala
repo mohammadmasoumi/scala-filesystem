ec48db13a659a5034aa1b18f0b2a494053f40d61
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-06 12:30:04 +0330

    refactor filesystem

--- src/com/mohammadmasoumi/scala/filesystem/FileSystem.scala
+++ src/com/mohammadmasoumi/scala/filesystem/FileSystem.scala
@@ -1,19 +1,22 @@
 package com.mohammadmasoumi.scala.filesystem
 
-import java.util.Scanner
-
 import com.mohammadmasoumi.scala.commands.Command
 import com.mohammadmasoumi.scala.files.Directory
 
 object FileSystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show
+    Command.from(newLine)(currentState)
+  })
 
-  while (true) {
-    state.show
-    val input = scanner.nextLine()
-    state = Command.from(input)(state) // state full program
-  }
+  //  var state = State(root, root)
+  //  val scanner = new Scanner(System.in)
+  //
+  //  while (true) {
+  //    state.show
+  //    val input = scanner.nextLine()
+  //    state = Command.from(input)(state) // state full program
+  //  }
 }
